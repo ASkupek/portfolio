68eb2ecfa35b57e6235057e8505f891f93851097
ASkupek <andraz.askupek@gmail.com>
2018-08-17 23:16:21 +0200

    Adding a blog.html and create a links.

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,6 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def text(self):
+        return self.body[:100]
